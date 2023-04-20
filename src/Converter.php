d00ffa5bf5e0e42f87ccf0baae324a94e65f494e
WaiPhyoNinja <waiphyohlaing445137@gmail.com>
2023-04-20 17:53:26 +0630

    simple package

---
+++ src/Converter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Wph\Engtomyan;
+
+use Illuminate\Support\Str;
+
+class Converter
+{
+    public function hello()
+    {
+        return 'Hello, world!';
+    }
+}
---
+++ tests/ConverterTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+use  Wph\Engtomyan\Converter;
+
+class ConverterTest 
+{
+    public function hello()
+    {
+        return 'Hello, world!';
+    }
+}
